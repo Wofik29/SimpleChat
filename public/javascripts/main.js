60e677fcb772ec0aec7dbe83c9df326b616c75b8
Danil <Linad29@mail.ru>
2017-11-11 16:16:00 +0500

    Make simple get and give message

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,13 @@
-var socket = io();
+$(function () {
+    var socket = io();
+    $('form').submit(function () {
+
+        socket.emit('chat message', $('#message').val());
+        $('#message').val('');
+        return false;
+    });
+
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<p>').text(msg));
+    })
+});
--- socket.js
+++ socket.js
@@ -3,4 +3,8 @@ var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
     console.log('USER CONNECTION');
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+        console.log('message: '+msg);
+    })
 });
